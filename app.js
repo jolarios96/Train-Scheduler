5b2681865fb7ce3046376bbf0ffdf9ae71a943aa
Jordan Larios <jolarios96@gmail.com>
2018-11-29 21:21:29 -0800

    added separate 5 min update period

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-var globalClock = setInterval(function () {
-    $('#clock').text(moment().format('MMMM Do, h:mm a'));
-    
-    //next, update table entries via firebase data
+// initial clock render before interval kicks in
+$('#clock').text(moment().format('MMMM Do, h:mm:ss a'));
 
-}, 60000);
+var globalClock = setInterval(function () {
+    $('#clock').text(moment().format('MMMM Do, h:mm:ss a'));
+
+
+}, 1000);
+
+var updateInterval = setInterval(function () {
+    // update table entries every 5 min
+
+}, 300000); // 1000ms * 60 * 5
 
 $('#submit').on('click', function () {
     //create new table row
     var newEntry = $('<tr>');
-        
+
     //append new table data to row
 
     // JSON.stringify & send to firebase
